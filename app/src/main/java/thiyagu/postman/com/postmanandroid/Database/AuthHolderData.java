f5d21cf21112562962f9c34ee53b32cf749f5af5
Takucoder <thiyagu@programmer.net>
2018-06-01 18:43:50 +0530

    auth holder added

---
+++ app/src/main/java/thiyagu/postman/com/postmanandroid/Database/AuthHolderData.java
@@ -0,0 +1,25 @@
+package thiyagu.postman.com.postmanandroid.Database;
+
+import android.util.Log;
+
+/**
+ * Created by thiyagu on 6/1/2018.
+ */
+
+public class AuthHolderData {
+
+
+    private  String BasicAuth;
+
+
+    public String getBasicAuth() {
+        return BasicAuth;
+    }
+
+    public void setBasicAuth(String basicAuth) {
+        BasicAuth = basicAuth;
+
+
+        Log.v("settingauthdata",basicAuth);
+    }
+}
