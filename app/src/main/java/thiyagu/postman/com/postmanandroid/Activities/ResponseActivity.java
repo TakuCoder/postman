ebfc4bb8af1f4ebd6abe4f0d839cda8b31f10eb0
Taku <thiyagu@programmer.net>
2018-08-20 00:18:42 +0530

    response cardview added

--- app/src/main/java/thiyagu/postman/com/postmanandroid/Activities/ResponseActivity.java
+++ app/src/main/java/thiyagu/postman/com/postmanandroid/Activities/ResponseActivity.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
@@ -19,7 +20,7 @@ public class ResponseActivity extends Activity {
     CardView responsedata;
 
     TextView status, time, size;
-    LinearLayout frameLayout_responsetype, frameLayout_responsedata;
+    LinearLayout frameLayout_responsetype, frameLayout_responsedata,main_type;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,6 +31,8 @@ public class ResponseActivity extends Activity {
 
         responsedata = findViewById(R.id.card_view_responsedata);
         frameLayout_responsetype = findViewById(R.id.frame_ResType);
+
+        main_type = findViewById(R.id.main_type);
         frameLayout_responsedata = findViewById(R.id.frame_ResData);
         status = findViewById(R.id.status_textview);
         time = findViewById(R.id.time_textview);
@@ -39,26 +42,26 @@ public class ResponseActivity extends Activity {
         final String responsetext = prefs.getString("response", null);
         final String timevalue = prefs.getString("time", null);
         final String codevalue = prefs.getString("code", null);
-        status.setText(codevalue + " OK");
+        status.setText(codevalue+ " OK");
         time.setText("TIME " + timevalue + " ms");
-
-size.setText("630 bytes");
-        responsetype.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-
-                if (frameLayout_responsetype.getVisibility() == View.VISIBLE) {
-                    frameLayout_responsetype.setVisibility(View.GONE);
-
-                } else {
-
-                    frameLayout_responsetype.setVisibility(View.VISIBLE);
-
-                }
-
-            }
-        });
+        Log.v("asdsadasdasdsadasd",codevalue);
+        size.setText("630 bytes");
+//        responsetype.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//
+//                if (frameLayout_responsetype.getVisibility() == View.VISIBLE) {
+//                    frameLayout_responsetype.setVisibility(View.GONE);
+//
+//                } else {
+//
+//                    frameLayout_responsetype.setVisibility(View.VISIBLE);
+//
+//                }
+//
+//            }
+//        });
 
 
         responsedata.setOnClickListener(new View.OnClickListener() {
@@ -74,6 +77,13 @@ size.setText("630 bytes");
                 }
             }
         });
+        Log.v("codevalue is",codevalue);
+        Log.v("codevalue is","200");
+        if(String.valueOf(codevalue).trim().equals("200"))
+        {
+            main_type.setBackgroundColor(getResources().getColor(R.color.yellow_green));
+Log.v("asdsadsadsa","amhere");
+        }
     }
 
     @Override
