dbd6b78becb4077356d617108d12ea19c2dae4cb
Taku <thiyagu@programmer.net>
2018-09-30 23:09:43 +0530

    history finished

--- app/src/main/java/thiyagu/postman/com/postmanandroid/HistoryActivity.java
+++ app/src/main/java/thiyagu/postman/com/postmanandroid/HistoryActivity.java
@@ -2,7 +2,9 @@ package thiyagu.postman.com.postmanandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
@@ -13,8 +15,12 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
+import thiyagu.postman.com.postmanandroid.Activities.CityEvent;
 import thiyagu.postman.com.postmanandroid.Database.FeedReaderDbHelper;
 import thiyagu.postman.com.postmanandroid.PopupActivities.DifferentRowAdapter;
+
+import static thiyagu.postman.com.postmanandroid.HistoryClass.DATE_TYPE;
+import static thiyagu.postman.com.postmanandroid.HistoryClass.RESPONSE_TYPE;
 
 public class HistoryActivity extends AppCompatActivity {
     RecyclerView recyclerView;
@@ -28,9 +34,11 @@ public class HistoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_history);
 
 
-        recyclerView = findViewById(R.id.my_recycler_view);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+//
+//        recyclerView = findViewById(R.id.my_recycler_view);
+//        recyclerView.setHasFixedSize(true);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         //initializing the productlist
         historyClassList = new ArrayList<>();
@@ -47,8 +55,8 @@ public class HistoryActivity extends AppCompatActivity {
 //            datelist.add(value[2]);
 //
 //        }
-//
-//
+
+
 //        Set<String> non_dup_dates = new LinkedHashSet<>(datelist);
 //
 //        datelist.clear();
@@ -58,33 +66,34 @@ public class HistoryActivity extends AppCompatActivity {
 
         for (int k = 0; k < aa.size(); k++) {
 
-//            if(k==0)
-//            {
-//                        historyClassList.add(new HistoryClass("https://www.google.com", "200ms", "680", "200","22.00.00"));
-//
-//            }
-//            else
-//            {
-//
-//
-//
-//            }
+            if(k==0)
+            {
+                       // historyClassList.add(new HistoryClass("https://www.google.com", "200ms", "680", "200","22.00.00"));
+
+            }
+            else
+            {
+
+
+
+            }
 
             ArrayList<String> data = feedReaderDbHelper.getAllhistory(aa.get(k));
 
-            for (int u = 0; u < data.size(); u++) {
+            for (int u = 0; u < data.size(); u++)
+            {
 
 
                 String value[]=data.get(u).split("@@");
 
-
+Log.v("sdasdsasa",data.get(u));
 
                 if (u == 0) {
-                    historyClassList.add(new HistoryClass(value[2],"","","","","","",0));
-                    historyClassList.add(new HistoryClass(value[1],value[2],value[3],value[4],value[5],value[6],value[7],1));
+                    historyClassList.add(new HistoryClass("",value[2],"","","","","", CityEvent.CITY_TYPE));
+                    historyClassList.add(new HistoryClass(value[1],value[2],value[3],value[4],value[5],value[6],value[7], CityEvent.EVENT_TYPE));
 
                 } else {
-                    historyClassList.add(new HistoryClass(value[1],value[2],value[3],value[4],value[5],value[6],value[7],1));
+                    historyClassList.add(new HistoryClass(value[1],value[2],value[3],value[4],value[5],value[6],value[7],CityEvent.EVENT_TYPE));
 
                 }
 
@@ -93,17 +102,17 @@ public class HistoryActivity extends AppCompatActivity {
 
         }
 
-
+        DifferentRowAdapter adapter = new DifferentRowAdapter(historyClassList);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, OrientationHelper.VERTICAL, false);
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setAdapter(adapter);
         //adding some items to our list
 //        historyClassList.add(
-//                new HistoryClass(
-//                        1,
-//                        "https://www.google.com",
-//                        "200ms",
-//                        "680",
-//
-//                        "200","22.00.00"));
-
+//                new HistoryClass("asaaasass","","","","","","",DATE_TYPE));
+//        historyClassList.add(
+//                new HistoryClass("asaaasass","","","","","","",RESPONSE_TYPE));
         //adding some items to our list
 //        historyClassList.add(
 //                new HistoryClass(
@@ -127,10 +136,27 @@ public class HistoryActivity extends AppCompatActivity {
 
 
         //creating recyclerview adapter
-        DifferentRowAdapter adapter = new DifferentRowAdapter(historyClassList);
+       // DifferentRowAdapter adapter = new DifferentRowAdapter(getData());
 
         //setting adapter to recyclerview
-        recyclerView.setAdapter(adapter);
+       // recyclerView.setAdapter(adapter);
+
 
     }
+
+
+
+        public static List<HistoryClass> getData() {
+            List<HistoryClass> list = new ArrayList<>();
+            list.add(new HistoryClass("London", "","","","","","", CityEvent.CITY_TYPE));
+            list.add(new HistoryClass("London", "","","","","","", CityEvent.EVENT_TYPE));
+            list.add(new HistoryClass("London", "","","","","","", CityEvent.EVENT_TYPE));
+            list.add(new HistoryClass("London", "","","","","","",  CityEvent.CITY_TYPE));
+            list.add(new HistoryClass("London", "","","","","","",  CityEvent.EVENT_TYPE));
+            list.add(new HistoryClass("London", "","","","","","",  CityEvent.CITY_TYPE));
+            list.add(new HistoryClass("London", "","","","","","",  CityEvent.EVENT_TYPE));
+            return list;
+        }
+
+
 }
--- app/src/main/java/thiyagu/postman/com/postmanandroid/PopupActivities/DifferentRowAdapter.java
+++ app/src/main/java/thiyagu/postman/com/postmanandroid/PopupActivities/DifferentRowAdapter.java
@@ -1,160 +1,243 @@
-package thiyagu.postman.com.postmanandroid.PopupActivities;
+package thiyagu.postman.com.postmanandroid.PopupActivities;//package thiyagu.postman.com.postmanandroid.PopupActivities;
+//
+//import android.content.Context;
+//import android.support.v7.widget.CardView;
+//import android.support.v7.widget.RecyclerView;
+//import android.util.Log;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.RelativeLayout;
+//import android.widget.TextView;
+//import android.widget.Toast;
+//
+//import java.util.List;
+//
+//import thiyagu.postman.com.postmanandroid.HistoryAdapter;
+//import thiyagu.postman.com.postmanandroid.HistoryClass;
+//import thiyagu.postman.com.postmanandroid.R;
+//
+//import static thiyagu.postman.com.postmanandroid.HistoryClass.DATE_TYPE;
+//import static thiyagu.postman.com.postmanandroid.HistoryClass.RESPONSE_TYPE;
+//
+//public class DifferentRowAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+//
+//    private Context mCtx;
+//
+//    //we are storing all the products in a list
+//    private List<HistoryClass> historyClassList;
+//
+//    public DifferentRowAdapter(List<HistoryClass> historyClassList) {
+//        this.historyClassList = historyClassList;
+//
+//    }
+//
+//    @Override
+//    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+//    {
+//        View view;
+//
+//        switch (viewType)
+//        {
+//
+//
+//            case DATE_TYPE:
+//
+////                inflater   = LayoutInflater.from(mCtx);
+////                view = inflater.inflate(R.layout.date, null);
+////                return new HistoryAdapter.DateViewHolder(view);
+//
+//                Log.v("whichview","1");
+//                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.date, parent, false);
+//                return new HistoryViewHolder(view);
+//
+//            case RESPONSE_TYPE:
+//                /**/
+//                Log.v("whichview","2");
+//                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_layout, parent, false);
+//                return new DateViewHolder(view);
+//
+//
+//
+////                inflater = LayoutInflater.from(mCtx);
+////                view = inflater.inflate(R.layout.cardview_layout, null);
+////                return new HistoryAdapter.HistoryViewHolder(view);
+//
+//
+//        }
+//        return null;
+//    }
+//
+//    class HistoryViewHolder extends RecyclerView.ViewHolder {
+//
+//        TextView url, duration, size, response_code, time;
+//        RelativeLayout imageView;
+//        CardView history_cardview;
+//
+//        public HistoryViewHolder(View itemView) {
+//            super(itemView);
+//            history_cardview = itemView.findViewById(R.id.history_cardview);
+//            time = itemView.findViewById(R.id.time_view);
+//            url = itemView.findViewById(R.id.url);
+//            size = itemView.findViewById(R.id.size);
+//            duration = itemView.findViewById(R.id.duration);
+//            response_code = itemView.findViewById(R.id.responsecode);
+//            imageView = itemView.findViewById(R.id.imageView);
+//        }
+//    }
+//
+//
+//    class DateViewHolder extends RecyclerView.ViewHolder {
+//
+//        TextView titleTextView;
+//
+//        public DateViewHolder(View itemView) {
+//            super(itemView);
+//
+//
+//
+//            titleTextView = itemView.findViewById(R.id.titleTextView);
+//
+//        }
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
+//        //getting the historyClass of the specified position
+//        final HistoryClass historyClass = historyClassList.get(position);
+//
+//        //binding the data with the viewholder views
+//        switch (historyClass.getmType()) {
+//            case DATE_TYPE:
+//                ((DateViewHolder) holder).titleTextView.setText(historyClass.getUrl());
+//                Log.v("whichview", "1");
+//                break;
+//
+//            case RESPONSE_TYPE:
+//                Log.v("whichview", "2");
+//                ((HistoryViewHolder) holder).url.setText(historyClass.getUrl());
+//                ((HistoryViewHolder) holder).duration.setText(historyClass.getDuration());
+//                ((HistoryViewHolder) holder).size.setText(String.valueOf(historyClass.getSize()));
+//                ((HistoryViewHolder) holder).history_cardview.setTag(historyClass.getTag());
+//                ((HistoryViewHolder) holder).response_code.setText(String.valueOf(historyClass.getResponse_code()));
+//                ((HistoryViewHolder) holder).imageView.setBackgroundColor(mCtx.getResources().getColor(R.color.green));
+//                ((HistoryViewHolder) holder).time.setText(String.valueOf(historyClass.getTime()));
+//                //holder.imageView.setImageDrawable(mCtx.getResources().getDrawable(historyClass.getResponse_code()));
+////holder.history_cardview.setOnLongClickListener(new View.OnLongClickListener() {
+////    @Override
+////    public boolean onLongClick(View view) {
+////        Toast.makeText(mCtx.getApplicationContext(),historyClass.getTag(),Toast.LENGTH_LONG).show();
+////        return true;
+////    }
+////});
+//
+//                Log.v("getdata", historyClass.getUrl() + " url" + "\n" +
+//                        historyClass.getDuration() + " duration" + "\n" +
+//                        historyClass.getSize() + " size" + "\n" +
+//                        historyClass.getTime() + " time" + "\n" +
+//                        historyClass.getResponse_code() + " responsecode" + "\n" +
+//                        historyClass.getTag() + " tag" + "\n" +
+//                        historyClass.getUrl() + " url" + "\n"
+//                );
+//                ((HistoryViewHolder) holder).history_cardview.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        Toast.makeText(mCtx, ((HistoryViewHolder) holder).history_cardview.getTag().toString(), Toast.LENGTH_LONG).show();
+//                    }
+//                });
+//                break;
+//
+//
+//        }
+//
+//
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return 0;
+//    }
+//}
 
-import android.content.Context;
-import android.support.v7.widget.CardView;
+
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 
-import thiyagu.postman.com.postmanandroid.HistoryAdapter;
+import thiyagu.postman.com.postmanandroid.Activities.CityEvent;
 import thiyagu.postman.com.postmanandroid.HistoryClass;
 import thiyagu.postman.com.postmanandroid.R;
 
-import static thiyagu.postman.com.postmanandroid.HistoryClass.DATE_TYPE;
-import static thiyagu.postman.com.postmanandroid.HistoryClass.RESPONSE_TYPE;
+import static thiyagu.postman.com.postmanandroid.Activities.CityEvent.CITY_TYPE;
+import static thiyagu.postman.com.postmanandroid.Activities.CityEvent.EVENT_TYPE;
 
 public class DifferentRowAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-
-    private Context mCtx;
-
-    //we are storing all the products in a list
-    private List<HistoryClass> historyClassList;
-
-    public DifferentRowAdapter(List<HistoryClass> historyClassList) {
-        this.historyClassList = historyClassList;
-
+    private List<HistoryClass> mList;
+    public DifferentRowAdapter(List<HistoryClass> list) {
+        this.mList = list;
     }
-
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
-    {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view;
-
-        switch (viewType)
-        {
-
-
-            case DATE_TYPE:
-
-//                inflater   = LayoutInflater.from(mCtx);
-//                view = inflater.inflate(R.layout.date, null);
-//                return new HistoryAdapter.DateViewHolder(view);
-
-                Log.v("whichview","1");
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.date, parent, false);
-                return new HistoryViewHolder(view);
-
-            case RESPONSE_TYPE:
-                /**/
-                Log.v("whichview","2");
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_layout, parent, false);
-                return new DateViewHolder(view);
-
-
-
-//                inflater = LayoutInflater.from(mCtx);
-//                view = inflater.inflate(R.layout.cardview_layout, null);
-//                return new HistoryAdapter.HistoryViewHolder(view);
-
-
+        switch (viewType) {
+            case CITY_TYPE:
+                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_item_city, parent, false);
+                return new CityViewHolder(view);
+            case EVENT_TYPE:
+                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_item_event, parent, false);
+                return new EventViewHolder(view);
         }
         return null;
     }
-
-    class HistoryViewHolder extends RecyclerView.ViewHolder {
-
-        TextView url, duration, size, response_code, time;
-        RelativeLayout imageView;
-        CardView history_cardview;
-
-        public HistoryViewHolder(View itemView) {
-            super(itemView);
-            history_cardview = itemView.findViewById(R.id.history_cardview);
-            time = itemView.findViewById(R.id.time_view);
-            url = itemView.findViewById(R.id.url);
-            size = itemView.findViewById(R.id.size);
-            duration = itemView.findViewById(R.id.duration);
-            response_code = itemView.findViewById(R.id.responsecode);
-            imageView = itemView.findViewById(R.id.imageView);
-        }
-    }
-
-
-    class DateViewHolder extends RecyclerView.ViewHolder {
-
-        TextView titleTextView;
-
-        public DateViewHolder(View itemView) {
-            super(itemView);
-
-
-
-            titleTextView = itemView.findViewById(R.id.titleTextView);
-
-        }
-    }
-
     @Override
-    public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
-        //getting the historyClass of the specified position
-        final HistoryClass historyClass = historyClassList.get(position);
-
-        //binding the data with the viewholder views
-        switch (holder.getItemViewType()) {
-            case DATE_TYPE:
-                ((DateViewHolder) holder).titleTextView.setText(historyClass.getUrl());
-                Log.v("whichview", "1");
-                break;
-
-            case RESPONSE_TYPE:
-                Log.v("whichview", "2");
-                ((HistoryViewHolder) holder).url.setText(historyClass.getUrl());
-                ((HistoryViewHolder) holder).duration.setText(historyClass.getDuration());
-                ((HistoryViewHolder) holder).size.setText(String.valueOf(historyClass.getSize()));
-                ((HistoryViewHolder) holder).history_cardview.setTag(historyClass.getTag());
-                ((HistoryViewHolder) holder).response_code.setText(String.valueOf(historyClass.getResponse_code()));
-                ((HistoryViewHolder) holder).imageView.setBackgroundColor(mCtx.getResources().getColor(R.color.green));
-                ((HistoryViewHolder) holder).time.setText(String.valueOf(historyClass.getTime()));
-                //holder.imageView.setImageDrawable(mCtx.getResources().getDrawable(historyClass.getResponse_code()));
-//holder.history_cardview.setOnLongClickListener(new View.OnLongClickListener() {
-//    @Override
-//    public boolean onLongClick(View view) {
-//        Toast.makeText(mCtx.getApplicationContext(),historyClass.getTag(),Toast.LENGTH_LONG).show();
-//        return true;
-//    }
-//});
-
-                Log.v("getdata", historyClass.getUrl() + " url" + "\n" +
-                        historyClass.getDuration() + " duration" + "\n" +
-                        historyClass.getSize() + " size" + "\n" +
-                        historyClass.getTime() + " time" + "\n" +
-                        historyClass.getResponse_code() + " responsecode" + "\n" +
-                        historyClass.getTag() + " tag" + "\n" +
-                        historyClass.getUrl() + " url" + "\n"
-                );
-                ((HistoryViewHolder) holder).history_cardview.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Toast.makeText(mCtx, ((HistoryViewHolder) holder).history_cardview.getTag().toString(), Toast.LENGTH_LONG).show();
-                    }
-                });
-                break;
-
-
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        HistoryClass object = mList.get(position);
+        if (object != null) {
+            switch (object.getmType()) {
+                case CITY_TYPE:
+                    ((CityViewHolder) holder).url.setText(object.getDate());
+                    break;
+                case EVENT_TYPE:
+                    ((EventViewHolder) holder).url.setText(object.getUrl());
+                    ((EventViewHolder) holder).method.setText(object.getReqtype());
+                    break;
+            }
         }
-
-
     }
-
     @Override
     public int getItemCount() {
+        if (mList == null)
+            return 0;
+        return mList.size();
+    }
+    @Override
+    public int getItemViewType(int position) {
+        if (mList != null) {
+            HistoryClass object = mList.get(position);
+            if (object != null) {
+                return object.getmType();
+            }
+        }
         return 0;
+    }
+    public static class CityViewHolder extends RecyclerView.ViewHolder {
+        private TextView url;
+        public CityViewHolder(View itemView) {
+            super(itemView);
+            url = (TextView) itemView.findViewById(R.id.url);
+        }
+    }
+    public static class EventViewHolder extends RecyclerView.ViewHolder {
+        private TextView url;
+        private TextView method;
+        public EventViewHolder(View itemView) {
+            super(itemView);
+            url = (TextView) itemView.findViewById(R.id.url);
+            method = (TextView) itemView.findViewById(R.id.method);
+        }
     }
 }
