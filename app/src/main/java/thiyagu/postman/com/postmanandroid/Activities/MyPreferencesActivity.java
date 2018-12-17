c1cbc721059707eb21d63f2a73967f56f0bdfede
Taku <thiyagu@programmer.net>
2018-12-18 00:03:54 +0530

    added cert import

--- app/src/main/java/thiyagu/postman/com/postmanandroid/Activities/MyPreferencesActivity.java
+++ app/src/main/java/thiyagu/postman/com/postmanandroid/Activities/MyPreferencesActivity.java
@@ -1,76 +1,112 @@
 package thiyagu.postman.com.postmanandroid.Activities;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.nbsp.materialfilepicker.MaterialFilePicker;
+import com.nbsp.materialfilepicker.ui.FilePickerActivity;
 
 import java.io.File;
+import java.util.regex.Pattern;
 
 import thiyagu.postman.com.postmanandroid.R;
 
 public class MyPreferencesActivity extends PreferenceActivity {
+    public static final int PERMISSIONS_REQUEST_CODE = 0;
+    public static final int FILE_PICKER_REQUEST_CODE = 1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
     }
 
-    public static class MyPreferenceFragment extends PreferenceFragment
-    {
+    public static class MyPreferenceFragment extends PreferenceFragment {
+      static  Preference filePicker;
+
         @Override
-        public void onCreate(final Bundle savedInstanceState)
-        {
+        public void onCreate(final Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
-            Preference filePicker = findPreference("filePicker");
+            filePicker = findPreference("filePicker");
             filePicker.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
-                public boolean onPreferenceClick(Preference preference)
-                {
-                    Intent intent = new Intent();
-                    intent.setType("file/*");
-                    intent.setAction(Intent.ACTION_GET_CONTENT);
-                    startActivityForResult(Intent.createChooser(intent, "Select Picture"), 200);
+                public boolean onPreferenceClick(Preference preference) {
+//                    Intent intent = new Intent();
+//                    intent.setType("file/*");
+//                    intent.setAction(Intent.ACTION_GET_CONTENT);
+//                    startActivityForResult(Intent.createChooser(intent, "Select Picture"), 200);
+//
+//
 
-
-
+                    checkPermissionsAndOpenFilePicker();
 
                     return true;
                 }
             });
         }
 
-        @Override
-        public void onActivityResult(int requestCode, int resultCode, Intent data) {
-            super.onActivityResult(requestCode, resultCode, data);
 
-//            String newValue = ....;
-//            SharedPreferences preferences = ......;
-//            SharedPreferences.Editor editor = preferences.edit();
-//            editor.putString("filePicker", newValue);
-//            editor.commit();
+        private void checkPermissionsAndOpenFilePicker() {
+            String permission = Manifest.permission.READ_EXTERNAL_STORAGE;
 
-
-            if (requestCode == 200 && resultCode == -1){
-                Uri content_describer = data.getData();
-                String src = content_describer.getPath();
-                File  source = new File(src);
-                Log.d("src is ", source.toString());
-                String filename = content_describer.getLastPathSegment();
-
-                Log.d("FileName is ",filename);
-                //destination = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Test/TestTest/" + filename);
-               // Log.d("Destination is ", destination.toString());
-               // SetToFolder.setEnabled(true);
+            if (ContextCompat.checkSelfPermission(getActivity(), permission) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), permission)) {
+                    showError();
+                } else {
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{permission}, PERMISSIONS_REQUEST_CODE);
+                }
+            } else {
+                openFilePicker();
             }
         }
+
+        private void openFilePicker() {
+            new MaterialFilePicker()
+                    .withActivity(
+                            getActivity()
+                    )
+                    .withRequestCode(FILE_PICKER_REQUEST_CODE)
+                    .withHiddenFiles(true)
+                    .withTitle("Sample title")
+                    .start();
+        }
+
+        private void showError() {
+
+            Toast.makeText(getActivity(), "Allow external storage reading", Toast.LENGTH_SHORT).show();
+        }
+        public Preference getMyPref(){
+            return filePicker;
+        }
+
+
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode == FILE_PICKER_REQUEST_CODE && resultCode == RESULT_OK) {
+            String path = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
+            Log.v("dsdsd", path);
+            MyPreferenceFragment.filePicker.setSummary(path);
+
+        }
+    }
 }
